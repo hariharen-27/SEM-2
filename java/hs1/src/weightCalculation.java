a1b96429e19fb8499b8d08f29b5fa7712552a229
hariharen-27 <hariharen27302@gmail.com>
2023-02-13 15:45:04 +0530

    edited in college

---
+++ java/hs1/src/weightCalculation.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class weightCalculation {
+    public static void main(String[] args){
+        int n,gender,height,weight;
+        Scanner input=new Scanner(System.in);
+        n=input.nextInt();
+
+
+
+
+    }
+}
