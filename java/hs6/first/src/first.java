1a3980d0037d6af6d2b7a7ff2e1af91f83fda5ac
hariharen-27 <hariharen27302@gmail.com>
2023-03-06 15:28:42 +0530

    edited hs6 first

--- java/hs6/first/src/first.java
+++ java/hs6/first/src/first.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class first {
+    public first(int a){
+        System.out.println(a);
+
+    }
     public static void main(String[] args){
+        first obj=new first(5);
+
 
     }
 }
