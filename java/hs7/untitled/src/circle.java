a077784cfd368b8964e47461c78a9fe956c5170a
hariharen-27 <hariharen27302@gmail.com>
2023-03-09 14:39:56 +0530

    added hs7

---
+++ java/hs7/untitled/src/circle.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class circle {
+    private double radius;
+    private String color = "red";
+
+    circle() {
+        this.radius = 5.0;
+    }
+
+    circle(double radius) {
+        this.radius = radius;
+    }
+
+    double getRadius() {
+        return this.radius;
+    }
+
+    double getArea(double radius) {
+        return 3.14*radius*radius;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+
+        circle c1 = new circle();
+        circle c2 = new circle(r);
+
+        System.out.println("Default Constructor Radius: " + c1.getRadius());
+        System.out.println("Area: " + c1.getArea(c1.getRadius()));
+        System.out.println("Parameter Radius " + c2.getRadius());
+        System.out.println("Area: " + c2.getArea(c2.getRadius()));
+
+
+    }
+}
