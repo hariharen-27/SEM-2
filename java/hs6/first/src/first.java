9a802d0b159cb5e516f50de5df1317643ed61b6f
hariharen-27 <hariharen27302@gmail.com>
2023-03-09 14:16:05 +0530

    added java files

--- java/hs6/first/src/first.java
+++ java/hs6/first/src/first.java
@@ -1,11 +1,22 @@
 import java.util.*;
 public class first {
-    public first(int a){
-        System.out.println(a);
+
+    void overloading(int a,int b ){
+        System.out.println(a+b);
 
     }
+    void overloading(int a){
+        System.out.println(a);
+    }
     public static void main(String[] args){
-        first obj=new first(5);
+
+        first obj=new first();
+        System.out.println("single argument");
+        obj.overloading(20);
+        System.out.println("double argument");
+        obj.overloading(20,20);
+
+
 
 
     }
